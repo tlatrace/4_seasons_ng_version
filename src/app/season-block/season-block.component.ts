9062862dcce55179df98b61954bd927e00bf588e
thibault <thibault.latrace@gmail.com>
2020-11-15 20:58:13 +0100

    css to scss migration

--- src/app/season-block/season-block.component.ts
+++ src/app/season-block/season-block.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'season-block',
   templateUrl: './season-block.component.html',
-  styleUrls: ['./season-block.component.css']
+  styleUrls: ['./season-block.component.scss']
 })
 export class SeasonBlockComponent implements OnInit {
 
--- src/app/season-block/season-date/season-date.component.ts
+++ src/app/season-block/season-date/season-date.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'season-date',
   templateUrl: './season-date.component.html',
-  styleUrls: ['./season-date.component.css']
+  styleUrls: ['./season-date.component.scss']
 })
 export class SeasonDateComponent implements OnInit {
 
